17cdf9cb1bd43b8641953ff3544f1cdf857797fa
achilinski <chilinskiadam1@gmail.com>
2021-12-16 22:48:01 +0100

    poprawiono bledu w pliku gauss.c oraz podzielono kod na funkcje

--- src/gauss.c
+++ src/gauss.c
@@ -5,44 +5,57 @@
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-
-int eliminate(Matrix *mat, Matrix *b){
-	int nr = mat->r;
-	int nc = mat->c;
-	double **A = mat->data;
-	double **B = b->data;
-	if (nr!=nc)
-		return -1;
-
-	for(int i=0; i<nc ; i++ ) {
-		double max = fabs(A[i][0]);
-		int ind = 0;
-		for (int t=i; t < nr; t++) {
-			if ( fabs(A[t][i]) > max ) {
-				max = fabs(A[t][i]);
-				ind = t;
-			}
+int max_element(Matrix *mat, int col) {
+	double maxi = -(double)(INFINITY -1);
+	int row = col;
+	for ( int i=col; i<mat->r; i++) {
+		if (mat->data[i][col]>maxi) {
+			row = i;
+			maxi = mat->data[i][col];
+		} else if (-mat->data[i][col]>maxi) {
+			row = i;
+			maxi = -mat->data[i][col];
 		}
-		double *tmp = A[ind];
-		A[ind] = A[i];
-		A[i] = tmp;
+	}
+	return row;
+}
 
-		double *tmpb = B[ind];
-		B[ind] = B[i];
-		B[i] = tmpb;
 
-		for(int j=i+1; j<nr; j++) {
-			if ( A[i][i] == 0 ) 
-				return 1;
-			double q = A[j][i]/A[i][i];
+void swap(Matrix *mat, int row, Matrix *b) {
+	int sw = max_element(mat, row);
+	double temp;
 
-			for ( int k = 0; k<nc; k++) {
+	for (int i=0; i<mat->r; i++) {
+		temp = mat->data[row][i];
+		mat->data[row][i]=mat->data[sw][i];
+		mat->data[row][i]=temp;
+	}
+
+	temp = b->data[row][0];
+	b->data[row][0]=b->data[row][0];
+	b->data[row][0]=temp;
+
+}
+int eliminate(Matrix *mat, Matrix *b){
+	int nr=mat->r;
+	int nc=mat->c;
+	double **A=mat->data;
+	double q;
+
+	for (int i=0; i<nc; i++) {
+		swap(mat, i, b);
+
+		if (A[i][i] == 0) 
+			return 1;
+
+		for (int j=i+1; j<nr; j++) {
+			q=A[j][i]/A[i][i];
+
+			for (int k=0; k<nc; k++ ) {
 				A[j][k] = A[j][k] - q*A[i][k];
 			}
-			B[j][0] = B[j][0] - q*B[j][0];	
-			
+			b->data[j][0]=b->data[j][0] - q*b->data[i][0];
 		}
-
-	}	
+	}
 	return 0;
-}	
+}
