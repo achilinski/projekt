307bfc1b3855ecf49dc3d1a11fb377f5ff8d9f05
michalaka123 <xcs72v2@gmail.com>
2021-12-16 03:05:12 +0100

    Dodana funkcja podstawienia wstecznego (backsubst)

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,16 +5,23 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	
+	if(x->c != 1 || b->c != 1 || mat->r != mat->c || mat->r != x->r || mat->r != b->r)
+		return 2;
+	for(i = mat->r; i >= 0; i--) {
+	       double sum = 0.0;
+	       for(j = i+1; j< mat->r; j++)
+			sum += mat->dane[i][j]*x->dane[j][0];
+	       if(mat->dane[i][i] == 0)
+		       return 1;
+	       x->dane[i][0] = (b->dane[i][0]-sum)/m->dane[i][i];
+	
+				//to ponizej jest przepisaniem b do x. Nalezy to poprawic! /
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+//				int i;
+//				for (i =0; i < x->r; i++) {
+//								x->data[i][0] = b->data[i][0];
+//				}
 
 				return 0;
 }
