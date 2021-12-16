a3164ddbdd60735354593c1237770c16fcd71c2b
achilinski <chilinskiadam1@gmail.com>
2021-12-16 20:55:50 +0100

    bieda debugging

--- src/main.c
+++ src/main.c
@@ -7,18 +7,24 @@
 
 int main(int argc, char ** argv) {
 	int res;
+	printf("read");
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
 	if (A == NULL) return -1;
 	if (b == NULL) return -2;
+	printf("print");
 	printToScreen(A);
 	printToScreen(b);
-
+	
+	printf("zaczynam eliminacje");
 	res = eliminate(A,b);
+	printf("wykonano eliminate");
 	x = createMatrix(b->r, 1);
+	printf("strworznow matix");
 	if (x != NULL) {
 		res = backsubst(x,A,b);
+		printf("wykonano backstub");
 
 		printToScreen(x);
 	  freeMatrix(x);
