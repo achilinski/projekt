d7919c97f084bda8b7b7e6f4651ec0dfb0c3eed7
achilinski <chilinskiadam1@gmail.com>
2021-12-16 23:34:52 +0100

    gauss z master 2 i czysty main

--- src/gauss.c
+++ src/gauss.c
@@ -37,7 +37,6 @@ void swap(Matrix *mat, int row, Matrix *b) {
 
 }
 int eliminate(Matrix *mat, Matrix *b){
-<<<<<<< HEAD
 	int nr=mat->r;
 	int nc=mat->c;
 	double **A=mat->data;
@@ -52,42 +51,7 @@ int eliminate(Matrix *mat, Matrix *b){
 		for (int j=i+1; j<nr; j++) {
 			q=A[j][i]/A[i][i];
 
-			for (int k=0; k<nc; k++ ) {
-=======
-	int nr = mat->r;
-	int nc = mat->c;
-	double **A = mat->data;
-	double **B = b->data;
-printf("\ninicjacja zmiennych\n");
-	if (nr!=nc)
-		return -1;
-printf("\nreturn -1 widze\n");
-	for(int i=0; i<nc ; i++ ) {
-		double max = fabs(A[i][0]);
-		int ind = 0;
-		for (int t=i; t < nr; t++) {
-printf("trafilem na fora");		
-			if ( fabs(A[t][i]) > max ) {
-				max = fabs(A[t][i]);
-				ind = t;
-			}
-		}
-printf("\n robie se jakis if\n");	
-		double *tmp = A[ind];
-		A[ind] = A[i];
-		A[i] = tmp;
-printf("\nrobie jakies podmiany w A\n");
-		double *tmpb = B[ind];
-		B[ind] = B[i];
-		B[i] = tmpb;
-printf("\nrobie jakies podmiany w B\n");
-		for(int j=i+1; j<nr; j++) {
-			if ( A[i][i] == 0 ) 
-				return 1;
-			double q = A[j][i]/A[i][i];
-printf("\nsam nie wiem co\n");
-			for ( int k = 0; k<nc; k++) {
->>>>>>> 7439d5e1eb6450e36fab413e1cf24eb3e91b30a5
+			for (int k=0; k<nc; k++ ){
 				A[j][k] = A[j][k] - q*A[i][k];
 			}
 			b->data[j][0]=b->data[j][0] - q*b->data[i][0];
